067383d8b32a7c9d44bd7961f2111614efcd52b9
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-04-03 11:48:19 +0700

    Fix test with infura, instead of running local IPFS

--- migrations/pushABIToIpfs.js
+++ migrations/pushABIToIpfs.js
@@ -6,15 +6,18 @@ const CONTRACT_ABI_DIR = "build/contracts"
 const abiPath = `${projectPath}/${CONTRACT_ABI_DIR}`;
 
 // Ipfs client conifg
-// const host = process.env.IPFS_HOST || "ipfs.infura.io"
-// const port = process.env.IPFS_PORT || 5001
-const host = process.env.IPFS_HOST || "localhost"
+const host = process.env.IPFS_HOST || "ipfs.infura.io"
 const port = process.env.IPFS_PORT || 5001
-const ipfsConfig = {host, port, protocol: "http"}
+const protocol = process.env.IPFS_PROTOCOL || "https"
+// const host = process.env.IPFS_HOST || "localhost"
+// const port = process.env.IPFS_PORT || 5001
+// const protocol = process.env.IPFS_PROTOCOL || "http"
+const ipfsConfig = {host, port, protocol}
 
 // ABI files
 const abiFilenames = fs.readdirSync(abiPath)
-const files = abiFilenames.map(fileName => `${abiPath}/${fileName}`)
+// This is not GOOD PRACTICE from INFURA, when they require data as buffer
+const files = abiFilenames.map(fileName => fs.readFileSync(`${abiPath}/${fileName}`))
 
 const pushABIToIpfs = () => {
   return pushFilesToIpfs(ipfsConfig, files)
