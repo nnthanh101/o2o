76170d924cf41edffd2a2b7ae178953723acaab8
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-04-10 09:35:43 +0700

    Add UserRegistry test

--- services/test/user-registry-service.test.js
+++ services/test/user-registry-service.test.js
@@ -1,25 +1,22 @@
-import { expect } from 'chai';
-import UserRegistryService from '../src/user-registry-service'
+import { expect } from "chai"
+import UserRegistryService from "../src/user-registry-service"
+import Web3 from "web3"
 
-const methodNames = [
-  'create',
-  'get'
-]
+const methodNames = ["create", "get"]
 
-describe('UserRegistryService', () => {
-
+describe("UserRegistryService", () => {
   let userRegistryService
 
   beforeEach(() => {
-    userRegistryService = new UserRegistryService()
+    let provider = new Web3.providers.HttpProvider("http://localhost:8545")
+    let web3 = new Web3(provider)
+    userRegistryService = new UserRegistryService({ web3 })
   })
 
-  methodNames.forEach((methodName) => {
+  methodNames.forEach(methodName => {
     it(`should have ${methodName} method`, () => {
       expect(userRegistryService[methodName]).to.be.an.instanceof(Function)
     })
   })
-
   // TODO: Real tests
-
 })
