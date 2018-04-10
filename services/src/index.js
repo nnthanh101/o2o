9fe9e6fa51d46310db825775f5eb7455e896cf52
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-04-10 10:21:52 +0700

    Fix missing 'ipfsConfig'

--- services/src/index.js
+++ services/src/index.js
@@ -11,13 +11,14 @@ const resources = {
 class O2OProtocol {
   constructor(options) {
     // Fail soon when no web3 supply
-    const { web3 } = options || {}
+    const { web3, ipfsConfig } = options || {}
     if (!web3) throw new Error("Please provide 'web3' in options")
+    if (!ipfsConfig) throw new Error("Please provide 'ipfsConfig' in options")
 
     // Should load web3 explicitly
     // Load from window.web3 is not good option
     this.contractService = new ContractService({ web3 })
-    this.ipfsService = new IpfsService({ web3 })
+    this.ipfsService = new IpfsService(ipfsConfig)
 
     // TODO: This service is deprecated
     this.userRegistryService = new UserRegistryService({ web3 })
