e3e65e74ad9509c8a9988fd7a42428295def9034
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-04-10 09:27:36 +0700

    Update test ipfs, still fail to connect local test

--- services/test/ipfs-service.test.js
+++ services/test/ipfs-service.test.js
@@ -1,37 +1,37 @@
-import { expect } from 'chai'
-import IpfsService from '../src/ipfs-service'
-import { listings, ipfsHashes } from './fixtures'
+import { expect } from "chai"
+import IpfsService from "../src/ipfs-service"
+import { listings, ipfsHashes } from "./fixtures"
 
-const clearCache = (ipfsService) => {
+const clearCache = ipfsService => {
   const { mapCache } = ipfsService
   Object.keys(mapCache.__data__).forEach(key => mapCache.del(key))
 }
 
-const methodNames = [
-  'submitFile',
-  'getFile',
-  'gatewayUrlForHash'
-]
+const methodNames = ["submitFile", "getFile", "gatewayUrlForHash"]
+const ipfsEnv = "local"
 
-const ipfsEnv = process.env.IPFS_DOMAIN ? 'local' : 'default'
-
-describe('IpfsService', () => {
-
+describe("IpfsService", () => {
   let ipfsService
 
   beforeEach(() => {
-    ipfsService = new IpfsService()
+    const ipfsConfig = {
+      ipfsDomain: "localhost",
+      ipfsApiPort: "5001",
+      ipfsGatewayPort: "8080",
+      ipfsProtocol: "http"
+    }
+    ipfsService = new IpfsService(ipfsConfig)
   })
 
-  methodNames.forEach((methodName) => {
+  methodNames.forEach(methodName => {
     it(`should have ${methodName} method`, () => {
       expect(ipfsService[methodName]).to.be.an.instanceof(Function)
     })
   })
 
-  describe('submitFile', () => {
+  describe("submitFile", () => {
     listings.forEach(({ data, ipfsHash }) => {
-      it('should successfully submit file', async () => {
+      it("should successfully submit file", async () => {
         const submittedHash = await ipfsService.submitFile(data)
         expect(submittedHash).to.equal(ipfsHash)
 
@@ -46,17 +46,17 @@ describe('IpfsService', () => {
     })
   })
 
-  describe('getFile', () => {
-    // Skipped because of https://github.com/o2oprotocol/o2oprotocol/issues/27
-    xit('should reject when listing cannot be found', (done) => {
-      ipfsService.getFile('QmWHyrPWQnsz1wxHR219ooJDYTvxJPyZuDUPSDpdsAovN5').then(done.fail, (error) => {
+  describe("getFile", () => {
+    // Skipped because of https://github.com/OriginProtocol/platform/issues/27
+    xit("should reject when listing cannot be found", done => {
+      ipfsService.getFile("QmWHyrPWQnsz1wxHR219ooJDYTvxJPyZuDUPSDpdsAovN5").then(done.fail, error => {
         expect(error).to.match(/Got ipfs cat error/)
         done()
       })
     })
   })
 
-  describe('gatewayUrlForHash', () => {
+  describe("gatewayUrlForHash", () => {
     ipfsHashes.forEach(({ ipfsHash, url }) => {
       it(`should correctly create url for IPFS hash ${ipfsHash}`, () => {
         const result = ipfsService.gatewayUrlForHash(ipfsHash)
@@ -64,5 +64,4 @@ describe('IpfsService', () => {
       })
     })
   })
-
 })
