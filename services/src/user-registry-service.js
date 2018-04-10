05b4372ede1fe5a3fadc9fb2ec7fe2701a6a6fa9
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-04-10 09:34:06 +0700

    Update UserRegistry service

--- services/src/user-registry-service.js
+++ services/src/user-registry-service.js
@@ -1,52 +1,60 @@
-import UserRegistryContract from "../build/contracts/UserRegistry.json"
+import UserRegistryContract from "../../build/contracts/UserRegistry.json"
 import promisify from "util.promisify"
 import contract from "truffle-contract"
 
 class UserRegistryService {
-  constructor() {
+  constructor(options) {
+    // Fail soon when no web3 supply
+    const { web3 } = options || {}
+    if (!web3) throw new Error("Please provide 'web3' in options")
+
+    this.web3 = web3
     this.userRegistryContract = contract(UserRegistryContract)
   }
 
-  //Creates a new user with attestation or proof payload data and stores in user-registry in relation to wallet ID
-  async create(payload) {
-    const { currentProvider, eth } = window.web3
-    const accounts = await promisify(eth.getAccounts.bind(eth))()
-
-    const walletId = accounts[0]
-    this.userRegistryContract.setProvider(currentProvider)
-
-    let instance
+  getUserRegistryInstance = async () => {
     try {
-      instance = await this.userRegistryContract.deployed()
+      const { userRegistryContract, web3 } = this
+      userRegistryContract.setProvider(web3.currentProvider)
+      return await userRegistryContract.deployed()
+    } catch (error) {
+      console.log(`Contract not deployed`)
+      throw error
+    }
+  }
+
+  //Creates a new user with attestation or proof payload data and stores in user-registry in relation to wallet ID
+  create = async payload => {
+    const { eth } = this.web3
+    const accounts = await promisify(eth.getAccounts.bind(eth))()
+    const walletId = accounts[0]
+
+    try {
+      const instance = await this.getUserRegistryInstance()
+      const response = instance.createAnother.call(walletId, JSON.stringify(payload))
+      console.log("user-registry-service found user:", response)
+      return response
     } catch (error) {
       console.log("user-registry-service could not find user:", walletId)
       throw error
     }
-
-    const response = instance.createAnother.call(walletId, JSON.stringify(payload))
-    console.log("user-registry-service found user:", response)
-    return response
   }
 
   //get user from from user-registry by their existing wallet ID
-  async get() {
-    const { currentProvider, eth } = window.web3
+  get = async () => {
+    const { eth } = this.web3
     const accounts = await promisify(eth.getAccounts.bind(eth))()
-
     const walletId = accounts[0]
-    this.userRegistryContract.setProvider(currentProvider)
 
-    let instance
     try {
-      instance = await this.userRegistryContract.deployed()
+      const instance = await this.getUserRegistryInstance()
+      const response = instance.get.call(walletId)
+      console.log("user-registry-service found user:", response)
+      return response
     } catch (error) {
       console.log("user-registry-service could not find user:", walletId)
       throw error
     }
-
-    const response = instance.get.call(walletId)
-    console.log("user-registry-service found user:", response)
-    return response
   }
 }
 
