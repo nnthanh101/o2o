664e755904bf3e59941c113da9966e0707a580b1
O2O Protocol <37603852+o2oprotocol@users.noreply.github.com>
2018-05-11 17:24:40 +0700

    Update ipfs.sh

--- docs/README/ipfs.sh
+++ docs/README/ipfs.sh
@@ -3,7 +3,7 @@
 
 # Read config
 API_PORT=${IPFS_ADDRES_API:-"5001"}
-GATEWAY_PORT=${IPFS_ADDRES_GATEWAY:-"8080"}
+GATEWAY_PORT=${IPFS_ADDRES_GATEWAY:-"8081"}
 
 # Update config
 ipfs config Addresses.API /ip4/0.0.0.0/tcp/$API_PORT
