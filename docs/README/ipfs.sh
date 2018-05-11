a40caeb9deb9c75e80cd4f3baebd40fbf897f135
O2O Protocol <37603852+o2oprotocol@users.noreply.github.com>
2018-05-11 17:21:05 +0700

    Create ipfs.sh

---
+++ docs/README/ipfs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+
+# Read config
+API_PORT=${IPFS_ADDRES_API:-"5001"}
+GATEWAY_PORT=${IPFS_ADDRES_GATEWAY:-"8080"}
+
+# Update config
+ipfs config Addresses.API /ip4/0.0.0.0/tcp/$API_PORT
+ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/$GATEWAY_PORT
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin "[\"*\"]"
+
+# Run daemon
+ipfs daemon --init
