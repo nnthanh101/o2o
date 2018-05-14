657db68c73c1a20842323540c8a87fbc4738fc0e
O2O <O2OProtocol101@gmail.com>
2018-05-14 11:52:52 +0700

    upgrade

--- contracts/migrations/3_create_sample_listings.js
+++ contracts/migrations/3_create_sample_listings.js
@@ -5,7 +5,6 @@ const fs = require("fs");
 const path = require("path");
 
 
-
 module.exports = function(deployer, network) {
   return deployer.then(() => {
     return deploy_sample_contracts(network)
@@ -48,37 +47,6 @@ async function deploy_sample_contracts(network) {
   console.log(`a_buyer_account:       ${a_buyer_account}`)
   console.log(`another_buyer_account: ${another_buyer_account}`)
 
-  // await listingsRegistry.create(
-  //   "0x4f32f7a7d40b4d65a917926cbfd8fd521483e7472bcc4d024179735622447dc9",
-  //   web3.toWei(3, "ether"),
-  //   1,
-  //   { from: a_seller_account, gas: 4476768 }
-  // )
-
-  // await listingsRegistry.create(
-  //   "0xa183d4eb3552e730c2dd3df91384426eb88879869b890ad12698320d8b88cb48",
-  //   web3.toWei(0.6, "ether"),
-  //   1,
-  //   { from: default_account, gas: 4476768 }
-  // )
-  // await listingsRegistry.create(
-  //   "0xab92c0500ba26fa6f5244f8ba54746e15dd455a7c99a67f0e8f8868c8fab4a1a",
-  //   web3.toWei(8.5, "ether"),
-  //   1,
-  //   { from: a_seller_account, gas: 4476768 }
-  // )
-  // await listingsRegistry.create(
-  //   "0x6b14cac30356789cd0c39fec0acc2176c3573abdb799f3b17ccc6972ab4d39ba",
-  //   web3.toWei(1.5, "ether"),
-  //   1,
-  //   { from: default_account, gas: 4476768 }
-  // )
-  // const ticketsTransaction = await listingsRegistry.create(
-  //   "0xff5957ff4035d28dcee79e65aa4124a4de4dcc8cb028faca54c883a5497d8917",
-  //   web3.toWei(0.3, "ether"),
-  //   27,
-  //   { from: default_account, gas: 4476768 }
-  // )
   const listingSample = path.join(__dirname, "..", "..", "build", "sample-data", "listings.json");
   const hasSampleFile = fs.exists(listingSample)
   
